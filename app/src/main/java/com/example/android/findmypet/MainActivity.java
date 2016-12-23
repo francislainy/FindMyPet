9bf637cb2e7809e878c8d01ac915666736f75d6c
Francislainy <francislainy.campos@gmail.com>
2016-12-23 14:57:03 +0000

    Transparent Icon set and centered image RelativeLayout

--- app/src/main/java/com/example/android/findmypet/MainActivity.java
+++ app/src/main/java/com/example/android/findmypet/MainActivity.java
@@ -3,19 +3,9 @@ package com.example.android.findmypet;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,23 +14,6 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.mipmap.ic_launcher_cat);
-
-//        btn = (Button) findViewById(R.id.button1);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //Clear the title
-//               // getActionBar().setTitle(null);
-//               // getActionBar().setSubtitle(null);
-//
-//                //Set custom view
-//                ImageView img = new ImageView(getApplicationContext());
-//                img.setImageResource(R.drawable.cat_paw);
-//                getSupportActionBar().setCustomView(img, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-//                getActionBar().setDisplayShowCustomEnabled(true);
-//
-//            }
-//        });
 
     }
 
