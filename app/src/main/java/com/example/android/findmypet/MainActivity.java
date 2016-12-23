faa6d698beb8fd5fe81a3be56a224d991c1e75ec
Francislainy <francislainy.campos@gmail.com>
2016-12-23 14:54:00 +0000

    Transparent Icon set

--- app/src/main/java/com/example/android/findmypet/MainActivity.java
+++ app/src/main/java/com/example/android/findmypet/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.android.findmypet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher_cat);
+
+//        btn = (Button) findViewById(R.id.button1);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //Clear the title
+//               // getActionBar().setTitle(null);
+//               // getActionBar().setSubtitle(null);
+//
+//                //Set custom view
+//                ImageView img = new ImageView(getApplicationContext());
+//                img.setImageResource(R.drawable.cat_paw);
+//                getSupportActionBar().setCustomView(img, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+//                getActionBar().setDisplayShowCustomEnabled(true);
+//
+//            }
+//        });
+
     }
+
 }
